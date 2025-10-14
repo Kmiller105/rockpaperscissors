79891b4a4aa2f096a248a41abdbc70e200fbe405
Kimberly miller <135668982+Kmiller105@users.noreply.github.com>
2025-10-14 16:18:17 -0500

    update

--- rock.js
+++ rock.js
@@ -1,56 +1,58 @@
-const game = () => {
-    let pScore = 0;
-    let cScore= 0;
+ const game = () => {
+       let pScore = 0;
+       let cScore = 0;
 
     const startGame = () => {
-      const playBtn = document.querySelector(".intro button");
-      const introScreen = document.querySelector(".intro");
-      const match = document.querySelector(".match");
-
-      playBtn.addEventListener("click" , () =>  {
-        introScreen.classList.add("fadeOut");
-        match.classList.add("fadeIn");
- });
+        const playBtn = document.querySelector(".intro button");
+        const introScreen = document.querySelector(".intro");
+        const match = document.querySelector(".match");
+           platBtn.addEventListener("click",() => {
+            introScreen.classList.add("fadeOut");
+            
+       });
     };
     const playMatch =() =>{
         const options = document.querySelectorAll(".options button");
-        const playerHand = document.querySelector(".playerhand");
-        const computerhand = document.querySelector(".computer=hand");
-const computerOptions =["rock", "paper", "scissors"];
-const hands =document.querySelectorAll(".hands img");
-hands.forEach(hand =>{
-    hand.addEventListener("animationend", function(){
+         const playerHand = document.querySelector(".playerhand");
+          const computerhand = document.querySelector(".computer=hand");
+           const computerOptions =["rock", "paper", "scissors"];
+            const hands =document.querySelectorAll(".hands img");
+
+        hands.forEach(hand =>{
+        hand.addEventListener("animationend", function(){
         this.style.animation ="";
     });
 
 });
  
- options.forEach(option => {
+  options.forEach(option => {
     option.addEventListener("click", function(){
-const computerNumber  = Math.floor(Math.random() * 3);
+     const computerNumber  = Math.floor(Math.random() * 3);
 
 setTimeout(() =>{
     compareHands(this.textContent, computerChoice);
     playerHand.src =`.assets/${this.textContent}.png`;
     computerHand.src =`.assets/${computerChoice}.png`;
 
-},2000)
+      },2000)
 
 
-playerHand.style.animation ="shakePlayer 2s ease";
-computerHand.style.animation ="shakeComputer 2s ease";
- });
+         playerHand.style.animation ="shakePlayer 2s ease";
+         computerHand.style.animation ="shakeComputer 2s ease";
+     });
  });
 
     };
     const updateScore =() =>{
         const playerScore = document.querySelector(".player-score p");
         const computerScore = document.querySelector(".computer-score p");
+
         playerScore.textContent = pScore;
         computerScore.textContent = cScore;
     }
     const compareHands = (playerChoice, computerChoice) =>{
         const winner = document.querySelector(".winner");
+        
         if (playerChoice === computerChoice){
             winner.textContent = "Its A Tie";
             return;
@@ -93,9 +95,9 @@ computerHand.style.animation ="shakeComputer 2s ease";
     }
     }
     startGame();
-    platMatch();
+    
 };
 game();
 
  
-}
+};
